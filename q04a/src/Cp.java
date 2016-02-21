e4034f57d39c39de6b1f299f9e1cd4f7d48aff99
Chris Kimberley <ckimbe02@mail.bbk.ac.uk>
2016-02-21 23:17:42 +0000

    now using try with resources

--- q04a/src/Cp.java
+++ q04a/src/Cp.java
@@ -13,13 +13,11 @@ public class Cp {
 
     public static void main(String[] args) {
         Cp cp = new Cp();
-        System.out.println("File Copy Program");
-        System.out.println("Please enter 2 filenames: source & destination");
         if (args.length == 2) {
             cp.launch(args[0], args[1]);
         }
         else {
-            System.out.println("You didn't enter 2 filenames!");
+            System.out.println("You didn't enter 2 filenames! Source & Destination");
         }
     }
 
@@ -32,11 +30,8 @@ public class Cp {
             if (! new Scanner(System.in).nextLine().toUpperCase().equals("Y")) {
                 System.out.println(("OK - file won't be copied"));
             } else {
-                BufferedReader in = null;
-                PrintWriter out = null;
-                try {
-                    in = new BufferedReader(new FileReader(sourceFile));
-                    out = new PrintWriter(destinationFile);
+                try (BufferedReader in = new BufferedReader(new FileReader(sourceFile));
+                    PrintWriter out = new PrintWriter(destinationFile)) {
                     String line;
                     while ((line = in.readLine()) != null) {
                         out.println(line);
@@ -46,35 +41,10 @@ public class Cp {
                     System.out.println(source + " does not exist!");
                 } catch (IOException ex) {
                     ex.printStackTrace();
-                } finally {
-                    closeReader(in);
-                    if (out != null) {
-                        out.close();
-                    }
                 }
             }
         } else {
             System.out.println("Destination file doesn't exist!");
         }
     }
-
-    private void closeReader(Reader reader) {
-        try {
-            if (reader != null) {
-                reader.close();
-            }
-        } catch (IOException ex) {
-            ex.printStackTrace();
-        }
-    }
-
-    /*private void closeWriter(PrintWriter writer) {
-        try {
-            if (writer != null) {
-                writer.close();
-            }
-        } catch (NullPointerException ex) {
-            ex.printStackTrace();;
-        }
-    }*/
 }
