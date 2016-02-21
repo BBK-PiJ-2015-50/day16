ef620c077693afb02bf7324146d4cb7943d67e4a
Chris Kimberley <ckimbe02@mail.bbk.ac.uk>
2016-02-21 23:05:07 +0000

    Specified all the import java.io...

--- q04a/src/Cp.java
+++ q04a/src/Cp.java
@@ -1,7 +1,12 @@
 /**
  * Created by chris on 12/22/15.
  */
-import java.io.*;
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
+import java.io.PrintWriter;
 import java.util.Scanner;
 
 public class Cp {
