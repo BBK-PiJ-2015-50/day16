0e7660d8b277f53d80718100c7180f0f52725373
Chris Kimberley <ckimbe02@mail.bbk.ac.uk>
2016-02-21 22:29:54 +0000

    trying out alternative line of code - shorter...still acceptable?

--- q02/src/MkDir.java
+++ q02/src/MkDir.java
@@ -1,9 +1,9 @@
-import java.io.*;
+import java.io.File;
 
 public class MkDir {
 
     public static void main(String[] args) {
-        File newDirectory = new File(args[0]);
-        newDirectory.mkdir();
+        //File newDirectory = new File(args[0]);
+        new File(args[0]).mkdir();
     }
 }
